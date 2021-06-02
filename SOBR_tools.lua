f7f5f7574d4fc81e33d8fc138aef62f3f76b1cae
Vladik1234 <57064681+Vladik1234@users.noreply.github.com>
2021-06-02 23:10:03 +0300

    Add files via upload

--- SOBR_tools.lua
+++ SOBR_tools.lua
@@ -1385,13 +1385,12 @@ function e.onServerMessage(color, text)
       end)
   end
   if (text:find("ƒобро пожаловать на Evolve Role Play")) then
-    goupdate()
-  end
-  if (text:find("ƒобро пожаловать на Evolve Role Play")) then
     local _, id = sampGetPlayerIdByCharHandle(playerPed)
     name = sampGetPlayerNickname(id)
     if name ~= "Leo_Florenso" or name ~= "Howard_Harper" or name ~= "Aleksey_Tarasov" or name ~= "Valentin_Molo" or name ~= "Evan_Corleone" or name ~= "Kevin_Spencor" or name ~= "Brain_Spencor" or name ~= "Sergu_Sibov" or name ~= "Jimmy_Saints" or name ~= "Saibor_Ackerman" or name ~= "Michael_Fersize" or name ~= "Barbie_Bell" or name ~= "Boulevard_Bledov" or name ~= "Hieden_Bell" or name ~= "Christian_Hazard" or name ~= "Bogdan_Mishenko" or name ~= "Ashton_Edwards" or name ~= "Santiago_Belucci" or name ~= "Chris_Ludvig" then
       sampAddChatMessage(""..name..", доступ открыт.", 0x33AAFFFF)
+      wait(1000)
+      goupdate()
     else
       sampAddChatMessage(""..name..", в доступе отказано.", 0xFFB22222)
       thisScript():unload()
